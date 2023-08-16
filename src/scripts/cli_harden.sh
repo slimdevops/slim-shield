50f938c293285be0904ed3a0989e970b9a5de6b3
Sreejith <sreejiths@focaloid.com>
2023-08-16 16:46:12 +0530

    code cleanup

--- src/scripts/cli_harden.sh
+++ src/scripts/cli_harden.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "Hardening workflow: $WORKFLOW_ID"
 
 log_output=$(slim harden --timeout 30m  --id "$WORKFLOW_ID" 2>&1 | tee /dev/stderr)
-
 if [[ $log_output =~ \[instrument\]\ completed ]]; then
   echo "The image has been successfully hardened ($WORKFLOW_ID)."
 else
