36ff95b9d947a9194b338ba1d4ada1458712aaa8
Sreejith <sreejiths@focaloid.com>
2023-11-15 18:54:54 +0530

    Harden testing

--- src/scripts/cli_harden.sh
+++ src/scripts/cli_harden.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 log_output=$(slim harden --timeout 30m  --id "$WORKFLOW_ID" 2>&1 | tee /dev/stderr)
+echo "Harden Output $log_output"
 if [[ $log_output =~ \[instrument\]\ completed ]]; then
   echo "The image has been successfully hardened ($WORKFLOW_ID)."
 else
