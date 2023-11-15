d4543401bf2449dee58932149506a1caf60065b8
Sreejith <sreejiths@focaloid.com>
2023-11-15 19:45:04 +0530

    HARDEN RESPONSE CHANGED FROM SAAS

--- src/scripts/cli_harden.sh
+++ src/scripts/cli_harden.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 log_output=$(slim harden --timeout 30m  --id "$WORKFLOW_ID" 2>&1 | tee /dev/stderr)
-echo "Harden Output $log_output"
-if [[ $log_output =~ \[instrument\]\ completed ]]; then
+
+if [[ "$?" == "0" ]]; then
   echo "The image has been successfully hardened ($WORKFLOW_ID)."
 else
   echo "The image hardening failed ($WORKFLOW_ID)."
